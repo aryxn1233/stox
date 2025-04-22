4c2d8d0802122a49257fc0f71cf9b6639674b5c2
Aryan Thakur <aryxnrajpoot786@gmail.com>
2025-04-22 13:32:25 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Stox',
+  webDir: 'dist'
+};
+
+export default config;
